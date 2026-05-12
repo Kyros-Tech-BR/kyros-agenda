cfe95ea2384bc16137aba10283ea6db9fcd21930
Kyros Tech <contato@kyrostech.com.br>
2026-05-11 22:36:31 -0300

    Fix Supabase project URL

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.KYROS_CONFIG = {
-  supabaseUrl: "https://jjhuwetztchufvyguffi.supabase.co",
+  supabaseUrl: "https://ijhuweztchufvyguefii.supabase.co",
   supabaseAnonKey: "sb_publishable_VhGZlt7E7qTim9Eji_-V9A__1GwdD4t",
   demoBusinessId: "11111111-1111-1111-1111-111111111111"
 };
